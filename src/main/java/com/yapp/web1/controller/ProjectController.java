e915551fdce2e90825c0745a42b0d595055acc2f
KimJye <eoeogudgud@naver.com>
2019-03-08 17:01:45 +0900

    create : createProjectAPI

--- src/main/java/com/yapp/web1/controller/ProjectController.java
+++ src/main/java/com/yapp/web1/controller/ProjectController.java
@@ -1,13 +1,17 @@
 package com.yapp.web1.controller;
 
+import com.yapp.web1.domain.Orders;
 import com.yapp.web1.domain.Project;
+import com.yapp.web1.service.OrderService;
 import com.yapp.web1.service.ProjectService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 /**
  * Project Controller
@@ -22,20 +26,51 @@ import javax.validation.Valid;
 @RestController
 public class ProjectController {
 
-    private ProjectService projectService;
+    @Autowired
+    ProjectService projectService;
+
+    @Autowired
+    OrderService orderService;
 
     /**
-     * 프로젝트 생성
+     * 프로젝트 상세 페이지
+     * @param project_idx, user_idx
      *
-     * @param project 생성할 프로젝트 데이터
+     * @see /v1/api/project
+     */
+    @GetMapping("/project")
+    @ResponseStatus(value = HttpStatus.OK)//200.(요청이 성공적으로 되었습니다.리소스를 불러와서 메시지 바디에 전송되었습니다.)
+    public void getProject(@PathVariable final Long projectIdx, @PathVariable final Long userIdx){
+        //프로젝트 상세 페이지 task list..?
+    }
+
+    /**
+     * 프로젝트 생성 팝업 get
+     *
+     * @param userIdx(session)
+     * @see /v1/api/project
+     */
+    @GetMapping("/popProject")//url고민좀..
+    @ResponseStatus(value = HttpStatus.OK)//200.(요청이 성공적으로 되었습니다.리소스를 불러와서 메시지 바디에 전송되었습니다.)
+    public List<Orders> getCreateProject(){
+        return orderService.findNumber();  //기수 선택
+        //플랫폼 선택 Enum converter,,,projectType
+    }
+
+    /**
+     * 프로젝트 생성 팝업 post
+     *
+     * @param project 생성할 프로젝트 데이터, useridx 프로젝트 생성자Idx(session)
      * @exception Exception 이미 join된 유저 - 추후 수정
      *
      * @see /v1/api/project
      */
-    @PostMapping("/project")
-    public ResponseEntity createProject(@RequestBody final Project project){
-//        projectService.saveProject(project);
-        return new ResponseEntity(HttpStatus.CREATED);
+    @PostMapping("/project/{userIdx}")
+    @ResponseStatus(value = HttpStatus.CREATED)//201. (요청이 성공적이었으며 그 결과로 새로운 리소스가 생성)
+    public String createProject(@RequestBody final Project project,@PathVariable final Long userIdx){
+       long projectIdx = projectService.insertProject(project,userIdx);
+       System.out.println("project 생성Idx : "+projectIdx);
+        return "redirect:/v1/api/project/{projectIdx}/{userIdx}";
     }
 
     /**
--- src/main/java/com/yapp/web1/service/ProjectService.java
+++ src/main/java/com/yapp/web1/service/ProjectService.java
@@ -1,6 +1,11 @@
 package com.yapp.web1.service;
 
+import com.yapp.web1.domain.Orders;
+import com.yapp.web1.domain.Project;
+import com.yapp.web1.repository.OrderRepository;
+import com.yapp.web1.repository.ProjectRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,4 +13,20 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @AllArgsConstructor
 public class ProjectService {
+
+    @Autowired
+    ProjectRepository projectRepository;
+
+    public long insertProject(Project p, long userIdx){
+        Project project = new Project();
+        project.setType(p.getType());//프로젝트 타입
+        project.setName(p.getName());//프로젝트 이름
+        project.setOrders(new Orders());//프로젝트 기수
+        project.getOrders().setIdx(p.getOrders().getIdx());
+        project.getOrders().setNumber(p.getOrders().getNumber());
+        project.setCreateUserIdx(userIdx);// 프로젝트 생성자Idx
+        projectRepository.save(project);
+        System.out.println("프로젝트 생성됨");
+        return project.getIdx();
+    }
 }
