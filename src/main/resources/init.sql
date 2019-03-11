ad7a6d3af91ce6ad9613803f808ac0e151457915
koda <kdkyung57@gmail.com>
2019-03-11 15:57:08 +0900

    feat: Add Data

---
+++ src/main/resources/init.sql
@@ -0,0 +1,5 @@
+INSERT INTO ORDERS(orders_idx, number)  VALUES (1,10);
+INSERT INTO ORDERS(orders_idx, number)  VALUES (2,11);
+INSERT INTO ORDERS(orders_idx, number)  VALUES (3,12);
+INSERT INTO ORDERS(orders_idx, number)  VALUES (4,13);
+INSERT INTO ORDERS(orders_idx, number)  VALUES (5,14);
