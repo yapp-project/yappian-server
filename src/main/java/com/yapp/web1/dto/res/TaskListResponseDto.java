f3cd9b9d0bfa978e302ab681f8d9e835b3fbc77e
koda <kdkyung57@gmail.com>
2019-03-14 06:17:53 +0900

    feat: Some changes to task dto

--- src/main/java/com/yapp/web1/dto/res/TaskListResponseDto.java
+++ src/main/java/com/yapp/web1/dto/res/TaskListResponseDto.java
@@ -1,6 +1,5 @@
 package com.yapp.web1.dto.res;
 
-import com.yapp.web1.domain.User;
 import com.yapp.web1.domain.VO.TaskJob;
 import com.yapp.web1.domain.VO.TaskStatus;
 import lombok.Builder;
@@ -17,18 +16,18 @@ import java.util.List;
 public class TaskListResponseDto {
 
     private Long taskIdx; // 테스크 idx
-    private String task; // 테스크 제목
+    private String taskTitle; // 테스크 제목
     private TaskJob taskJob; // 테스크 직군
-    private List<User> userList; // 테스크 담당자
+    private List<String> userList; // 테스크 담당자
     private LocalDate startDate; // 테스크 기간 - 시작
     private LocalDate endDate; // 테스크 기간 - 끝
     private TaskStatus taskStatus; // 테스크 진행상태
 
     @Builder
-    public TaskListResponseDto(Long taskIdx, String task, TaskJob taskJob, List<User> userList,
+    public TaskListResponseDto(Long taskIdx, String taskTitle, TaskJob taskJob, List<String> userList,
                                LocalDate startDate, LocalDate endDate, TaskStatus taskStatus){
         this.taskIdx = taskIdx;
-        this.task = task;
+        this.taskTitle = taskTitle;
         this.taskJob = taskJob;
         this.userList = userList;
         this.startDate = startDate;
