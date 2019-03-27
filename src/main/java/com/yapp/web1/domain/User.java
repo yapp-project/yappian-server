a07e8ffc47031c6027613cc2c565c0ea3a85dcb4
koda <kdkyung57@gmail.com>
2019-03-28 08:35:42 +0900

    feat: Task-User 연관관계 Read Table 추가

--- src/main/java/com/yapp/web1/domain/User.java
+++ src/main/java/com/yapp/web1/domain/User.java
@@ -23,6 +23,7 @@ import java.util.*;
 @Getter
 public class User extends BaseEntity {
 
+    /** User Table Fields **/
     @Column(name="email", nullable=false, unique=true)
     private String email;
 
@@ -33,13 +34,24 @@ public class User extends BaseEntity {
     @Convert(converter = UserRoleAttributeConverter.class)
     private UserRole role = UserRole.USER;
 
+    /** Relation Mapping **/
+    /** User - UserOrders 양방향 매핑 **/
     @JsonIgnore
     @OneToMany(mappedBy="user",
             cascade = CascadeType.ALL,
             fetch = FetchType.LAZY,
             orphanRemoval = true)
-    private Set<UserOrder> userOrders = new HashSet<>();
+    private Set<UserOrders> userOrders = new HashSet<>();
 
+    /** User - Read 양방향 매핑 **/
+    @JsonIgnore
+    @OneToMany(mappedBy = "user",
+            cascade = CascadeType.REMOVE,
+            fetch = FetchType.LAZY, // EAGER
+            orphanRemoval = true)
+    private List<Read> readList = new ArrayList<>();
+
+    /** User - Comment 양방향 매핑 **/
     @JsonIgnore
     @OneToMany(mappedBy = "user",
             cascade = CascadeType.PERSIST,
@@ -47,7 +59,8 @@ public class User extends BaseEntity {
             orphanRemoval = true)
     private List<Comment> commentList = new ArrayList<>();
 
-
+    /** Relation Mapping - Join Table **/
+    /** User - Project 단방향 매핑 **/
     @OneToMany(fetch=FetchType.LAZY,
             cascade = CascadeType.REMOVE,//ALL
             orphanRemoval = true)
@@ -57,22 +70,26 @@ public class User extends BaseEntity {
     @OrderBy("project_idx desc")
     private Set<Project> favorites = new HashSet<>();
 
+    /** User - Project 단방향 매핑 **/
     @OneToMany(fetch=FetchType.LAZY,
             cascade = CascadeType.PERSIST,
             orphanRemoval = true)
-    @JoinTable(name="joined_project",
+    @JoinTable(name="joined",
             joinColumns = @JoinColumn(name="user_idx"),
             inverseJoinColumns = @JoinColumn(name="project_idx"))
     private Set<Project> joinedProjects = new HashSet<>();
 
+
+    /** Method **/
     @Builder
     public User(String email, String name, UserRole role,
-                Set<UserOrder> userOrders, List<Comment> commentList, Set<Project> favorites, Set<Project> joinedProject){
+                Set<UserOrders> userOrders, List<Read> readList, List<Comment> commentList, Set<Project> favorites, Set<Project> joinedProject){
         this.email = email;
         this.name = name;
         this.role = role;
 
         this.userOrders = Optional.ofNullable(userOrders).orElse(this.userOrders);
+        this.readList = Optional.ofNullable(readList).orElse(this.readList);
         this.commentList = Optional.ofNullable(commentList).orElse(this.commentList);
         this.favorites = Optional.ofNullable(favorites).orElse(this.favorites);
         this.joinedProjects = Optional.ofNullable(joinedProject).orElse(this.joinedProjects);
