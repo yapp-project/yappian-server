e357b872f82351dd19b4a2215bccba4cd18c2a94
koda <kdkyung57@gmail.com>
2019-05-10 02:32:32 +0900

    chore: modelmapper

---
+++ src/main/java/com/yapp/web1/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.yapp.web1.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
