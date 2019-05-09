4ad634e9254bf7c15a81241f0e058062df096a72
koda <kdkyung57@gmail.com>
2019-05-10 02:52:55 +0900

    fix: test error

--- src/test/java/com/yapp/web1/ApplicationTests.java
+++ src/test/java/com/yapp/web1/ApplicationTests.java
@@ -3,8 +3,14 @@ package com.yapp.web1;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@TestPropertySource(
+        properties = {
+                "spring.config.location=classpath:application.yml,/app/config/yappian/s3.yml"
+        }
+)
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
