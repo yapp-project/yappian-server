0e5ae4d754437dbd7ab226c2fa0f8f6b11067b5f
koda <kdkyung57@gmail.com>
2019-05-31 00:41:00 +0900

    chore: Deploy v0.13.21
    - Front v2.5 : xml request

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.10f70da443dd3b1990ee2070cf9a5e65.js"
+  "/precache-manifest.e244eb04a752f33f5cffa7b2572730db.js"
 );
 
 workbox.clientsClaim();
