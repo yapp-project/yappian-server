945032dc8104016af3e9da3b482c15ff370ad511
koda <kdkyung57@gmail.com>
2019-05-31 22:11:53 +0900

    feat: login check
    - login check url
    - logout delete cookie

--- src/main/java/com/yapp/web1/controller/AuthController.java
+++ src/main/java/com/yapp/web1/controller/AuthController.java
@@ -1,8 +1,15 @@
 package com.yapp.web1.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.session.FindByIndexNameSessionRepository;
+import org.springframework.session.Session;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
 
 import java.security.Principal;
 
@@ -14,12 +21,30 @@ import java.security.Principal;
  * @since 0.0.4
  * @version 1.0
  */
+@Api(tags = "인증체크 API")
 @CrossOrigin("*")
 @RestController
 public class AuthController {
 
+    @Autowired
+    FindByIndexNameSessionRepository<? extends Session> sessions;
+
+    @ApiOperation(value = "로그인 유저 데이터 확인")
     @GetMapping("/auth")
     public Principal home(Principal principal) {
         return principal;
+    }
+
+    @ApiOperation(value = "로그인 유저 세션 확인")
+    @GetMapping("/session")
+    public String currentSession(){
+        if(SecurityContextHolder.getContext().getAuthentication().getPrincipal() == "anonymousUser")
+            return "ANONYMOUS";
+
+        Session findSession = sessions.getSession(RequestContextHolder.currentRequestAttributes().getSessionId());
+        if(findSession != null)
+            return findSession.getId();
+
+        return "invalid";
     }
 }
